e7ddf68ca56ff59ea88eb3648d8cfe715f06351b
Cortez Christian <CortezChristian@rocketmortgage.com>
2021-12-03 12:32:20 -0500

    Code push

---
+++ BookLibrary/Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using BookLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Controllers
+{
+    public class BookController : Controller
+    {
+        public IActionResult Index()
+        {
+            Book model = new Book();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Book model)
+        {
+            return RedirectToAction("Confirmation", model);
+        }
+
+        public IActionResult Confirmation(Book model)
+        {
+            return View(model);
+        }
+    }
+}
